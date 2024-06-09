83721a0ca63da555744dabc0af058c499a204382
hojuncha997 <hojun.cha997@gmail.com>
2024-06-09 10:11:50 +0900

    feat: useNavigate 사용

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 const Loading = <div>Loading...</div>;
 const TodoList = lazy(() => import("../pages/todo/ListPage"));
 const TodoRead = lazy(() => import("../pages/todo/ReadPage"));
+const TodoAdd = lazy(() => import("../pages/todo/AddPage"));
 
 const todoRouter = () => {
   return [
@@ -26,6 +27,14 @@ const todoRouter = () => {
       element: (
         <Suspense fallback={Loading}>
           <TodoRead />
+        </Suspense>
+      ),
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={Loading}>
+          <TodoAdd />
         </Suspense>
       ),
     },
