f4ce5510a804f9df4620eba704dea9ed44277e21
차호준 <hjcha@markany.com>
2024-06-10 13:26:19 +0900

    feat: 등록 컴포넌트 생성(AddComponent.js)

---
+++ src/components/todo/AddComponent.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const initState = {
+  title: "",
+  writer: "",
+  dueDate: "",
+};
+
+const AddComponent = () => {
+  const [todo, setTodo] = useState({ ...initState });
+
+  const handleChangeTodo = (event) => {
+    todo[event.target.name] = event.target.value;
+    setTodo({ ...todo });
+  };
+
+  const handleClickAdd = () => {
+    console.log(todo);
+  };
+
+  return (
+    <div className="p-4 m-2 mt-10 border-2 border-sky-200">
+      <div className="flex justify-center">
+        <div className="relative flex flex-wrap items-stretch w-full mb-4">
+          <div className="w-1/5 p-6 font-bold text-right">TITLE</div>
+          <input
+            className="w-4/5 p-6 border border-solid rounded-r shadow-md border-neutral-500"
+            name="title"
+            type={"text"}
+            value={todo.title}
+            onChange={handleChangeTodo}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative flex flex-wrap items-stretch w-full mb-4">
+          <div className="w-1/5 p-6 font-bold text-right ">WRITER</div>
+          <input
+            className="w-4/5 p-6 border border-solid rounded-r shadow-md border-neutral-500"
+            name="writer"
+            type={"text"}
+            value={todo.writer}
+            onChange={handleChangeTodo}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="relative flex flex-wrap items-stretch w-full mb-4">
+          <div className="w-1/5 p-6 font-bold text-right ">WRITER</div>
+          <input
+            className="w-4/5 p-6 border border-solid rounded-r shadow-md border-neutral-500"
+            name="dueDate"
+            type={"text"}
+            value={todo.dueDate}
+            onChange={handleChangeTodo}
+          ></input>
+        </div>
+      </div>
+
+      <div className="flex justify-end">
+        <div className="relative flex flex-wrap items-stretch p-4 mb-4">
+          <button
+            type="button"
+            className="p-4 text-xl text-white bg-blue-500 rounded w-36"
+            onClick={handleClickAdd}
+          >
+            ADD
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddComponent;
