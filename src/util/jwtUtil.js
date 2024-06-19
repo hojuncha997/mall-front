6149be90104bd59f764b8c907081a8af4ff70f40
hjcha <hjcha@markany.com>
2024-06-19 16:52:33 +0900

    feat: (중요) Refresh Token을 이용한 자동갱신

--- src/util/jwtUtil.js
+++ src/util/jwtUtil.js
@@ -1,9 +1,36 @@
 import axios from 'axios';
-import { getCookie } from './cookieUtil';
+import { getCookie, setCookie } from './cookieUtil';
+import { API_SERVER_HOST } from '../api/todoApi';
 
 
 
 const jwtAxios = axios.create();
+
+
+// Access Token과 Refresh Token을 이용해서 새로운 Access Token을 발급받는 함수
+const refreshJWT = async (accessToken, refreshToken) => {
+    
+    const host = API_SERVER_HOST;
+
+    const header = {headers: {'Authorization': `Bearer ${accessToken}`}};
+
+    const res = await axios.get(`${host}/api/member/refresh?refreshToken=${refreshToken}`, header);
+
+    console.log("-------------------------------")
+    console.log(res.data)
+
+    return res.data;
+
+    /* 
+        beforeRes()에서 응답 데이터가 "ERROR_ACCESS_TOKEN"과 같이 
+        Access Token 관련 메시지인 경우 Refresh Token을 활용해서 다시 호출한다
+    */
+
+}
+
+
+
+
 
 // before request: 역할은 request가 보내기 전에 처리하는 것
 const beforeReq = (config) => {
@@ -31,8 +58,28 @@ const requestFail = (error) => {
 }
 
 // before return response: 역할은 response가 오기 전에 처리하는 것
-const beforeRes = (response) => {
+const beforeRes = async (response) => {
     console.log("before return response ........");
+
+    console.log(response);
+    const data = response.data;
+
+
+    if(data && data.error === "ERROR_ACCESS_TOKEN") {
+
+        const memberCookieValue = getCookie("member");
+
+        const result = await refreshJWT(memberCookieValue.accessToken, memberCookieValue.refreshToken);
+
+        console.log("refresh RESULT: ", result);
+
+        memberCookieValue.accessToken = result.accessToken;
+        memberCookieValue.refreshToken = result.refreshToken;
+
+        setCookie("member", JSON.stringify(memberCookieValue), 1);
+    }
+        
+
     return response;
 }
 
