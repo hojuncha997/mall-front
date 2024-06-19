80260b8bf7b40dc2d4838f79b7aba32b7cf92446
hjcha <hjcha@markany.com>
2024-06-19 14:42:50 +0900

    feat: 리덕스 툴킷: 로그인이 필요한 페이지의 처리(useCustomLogin사용)

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,16 @@
+import useCustomLogin from "../hooks/useCustomLogin";
 import BasicLayout from "../layouts/BasicLayout";
+
 const AboutPage = () => {
+
+  const {isLogin, moveToLoginReturn} = useCustomLogin();
+
+  // 로그인이 되어 있지 않다면 로그인 페이지로 이동한다. useCustomLogin() 훅을 이용했기 때문에 코드가 간결해진다.
+  if(!isLogin) {
+    return moveToLoginReturn();
+  }
+
+
   return (
     <BasicLayout>
       <div className="text-3xl">AboutPage</div>
