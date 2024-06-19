4bad78849826f2b9773e34ceea7696583f257dd4
hjcha <hjcha@markany.com>
2024-06-19 10:34:24 +0900

    feat: 리덕스 툴킷: 로그인 이후 로그인 버튼 사라지도록 처리

--- src/components/menus/BasicMenu.js
+++ src/components/menus/BasicMenu.js
@@ -34,7 +34,13 @@ const BasicMenu = () => {
       </div>
 
       <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
-        <div className="text-white text-sm m-1 rounded"><Link to={'/member/login'}>Login</Link></div>
+
+      {!loginState.email ? 
+      <div className="text-white text-sm m-1 rounded">
+        <Link to={'/member/login'}>Login</Link>
+        </div> : <></>
+      }
+        
       </div>
     </nav>
   );
