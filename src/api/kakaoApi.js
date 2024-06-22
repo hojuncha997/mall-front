1dfe8c9642aa7ccdcd74612caeea0fe403b64192
hojuncha997 <hojun.cha997@gmail.com>
2024-06-22 19:57:46 +0900

    feat: 리액트에서의 호출 테스트(카카오 -> 액세스 토큰 -> 백엔드)

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { API_SERVER_HOST } from "./todoApi";
 
 const rest_api_key = `ec922083d15a70f8304f17b2e289e21e`; // REST 키 값
 const redirect_uri = `http://localhost:3000/member/kakao`;
@@ -8,7 +8,6 @@ const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
 
 const access_token_url = `https://kauth.kakao.com/oauth/token`; // 추가
 
-
 export const getKakaoLoginLink = () => {
   const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
@@ -16,26 +15,33 @@ export const getKakaoLoginLink = () => {
 };
 
 export const getAcccessToken = async (authCode) => {
- 
   const header = {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
     },
-  }
+  };
 
   const params = {
     grant_type: "authorization_code",
     client_id: rest_api_key,
     redirect_uri: redirect_uri,
     code: authCode,
-  }
+  };
 
   const res = await axios.post(access_token_url, params, header);
 
   const accessToken = res.data.access_token;
 
   return accessToken;
-}
+};
+
+// 아래 함수는 카카오 로그인 결과로 전달되는 accessToken을 이용하여 사용자 정보를 가져오는 함수이다.
+export const getMemberWithAccessToken = async (accessToken) => {
+  const res = await axios.get(
+    `${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`
+  );
+  return res.data;
+};
 
 /*
   카카오 로그인 결과로 전달되는 인가코드는 Access Token을 받기 위한 사전작업이다.
--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,28 +1,31 @@
 import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom"
-import { getAcccessToken } from "../../api/kakaoApi";
+import { useSearchParams } from "react-router-dom";
+import { getAcccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
 
 const KakaoRedirectPage = () => {
+  const [searchParams] = useSearchParams();
 
-    const [searchParams] = useSearchParams();
+  const authCode = searchParams.get("code");
 
-    const authCode = searchParams.get("code");
+  useEffect(() => {
+    // authCode를 이용하여 accessToken을 가져온다.
+    getAcccessToken(authCode).then((accessToken) => {
+      console.log(accessToken);
 
+      // accessToken을 이용하여 사용자 정보를 가져온다.
+      getMemberWithAccessToken(accessToken).then((memberInfo) => {
+        console.log("------------------------");
+        console.log(memberInfo);
+      });
+    });
+  }, [authCode]);
 
-    useEffect(() => {
-        getAcccessToken(authCode)
-            .then((data) => {
-                console.log(data);
-            })
-    }, [authCode])
-
-
-    return (
-        <div>
-            <div>Kakao Login Redirect</div>
-            <div>{authCode}</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Kakao Login Redirect</div>
+      <div>{authCode}</div>
+    </div>
+  );
+};
 
 export default KakaoRedirectPage;
