ad0cf06784dfd2479fe1398f1f0fee761a8f6b2b
hojuncha997 <hojun.cha997@gmail.com>
2024-06-23 23:02:13 +0900

    feat: 장바구니 아이템 가져와서 숫자 보여주기

--- src/components/menus/CartComponent.js
+++ src/components/menus/CartComponent.js
@@ -1,11 +1,35 @@
+import { useEffect } from "react";
 import useCustomLogin from "../../hooks/useCustomLogin";
+import { getCartItemsAsync } from "../../slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartComponent = () => {
   const { isLogin, loginState } = useCustomLogin();
 
+  const dispatch = useDispatch();
+
+  const cartItems = useSelector((state) => state.cartSlice);
+
+  useEffect(() => {
+    if (isLogin) {
+      // 만약 로그인 한 상태라면 리듀서인 cartSlice에 정의된 getCartItemsAsync 함수를 호출한다.
+      // 이 함수는 장바구니 목록을 가져오는 API를 호출한다.
+      dispatch(getCartItemsAsync());
+    }
+  }, [isLogin]);
+
   return (
     <div className="w-full">
-      {isLogin ? <div>{loginState.nickname}'s Cart</div> : <></>}
+      {isLogin ? (
+        <div className="flex">
+          <div className="m-2 font-extrabold">{loginState.nickname}'s Cart</div>
+          <div className="bg-orange-600 w-9 text-center text-white font-bold rounded-full m-2">
+            {cartItems.length}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
