daf28d0c2656af027522ed7385c97807fc27e81a
hjcha <hjcha@markany.com>
2024-06-26 10:20:34 +0900

    feat:**********  Recoil 설치 및 적용**********

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+//Redux-toolkit
 import { Provider } from "react-redux";
 import store from "./store";
+
+// Recoil
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </Provider>
 );
 
