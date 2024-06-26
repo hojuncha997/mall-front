3c324250b7e60ead44aea69f76fc48c8700c9c8a
hjcha <hjcha@markany.com>
2024-06-26 13:23:40 +0900

    feat: 리코일 사용으로 인한 카카오 로그인 코드 수정

--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -4,13 +4,17 @@ import { getAcccessToken, getMemberWithAccessToken } from "../../api/kakaoApi";
 import useCustomLogin from "../../hooks/useCustomLogin";
 
 // API 서버에서 전송한 결과를 dispatch를 이용하여 login() 함수를 호출하여 상태를 변경한다.
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux"; // 리덕스 툴킷 사용으로 인한 주석 처리
 import { login } from "../../slices/loginSlice";
 
 const KakaoRedirectPage = () => {
+
   const [searchParams] = useSearchParams();
-  const dispatch = useDispatch();
-  const { moveToPath } = useCustomLogin(); // 이 부분을 컴포넌트 내부로 옮김
+  
+  // 리덕스 툴킷 사용으로 인한 주석 처리
+  // const dispatch = useDispatch();
+  
+  const { moveToPath, saveAsCookie } = useCustomLogin(); // 이 부분을 컴포넌트 내부로 옮김
 
   const authCode = searchParams.get("code");
 
@@ -23,7 +27,11 @@ const KakaoRedirectPage = () => {
       getMemberWithAccessToken(accessToken).then((memberInfo) => {
         console.log("------------------------");
         console.log(memberInfo);
-        dispatch(login(memberInfo)); // 이 결과로 쿠키가 생성된다.
+
+        // dispatch(login(memberInfo)); // 이 결과로 쿠키가 생성된다. // 리덕스 툴킷 사용으로 인한 주석 처리
+
+        // 리코일 사용으로 인한 변경. 직접 쿠키를 생성한다.
+        saveAsCookie(memberInfo); // 리코일 사용으로 인한 변경  
 
         // 소셜 회원이 아니라면
         if (memberInfo && !memberInfo.social) {
